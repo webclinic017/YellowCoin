cbfa1935d75e14d3390d8c2082ce158d2ad0361c
TheJagStudio <81472856+TheJagStudio@users.noreply.github.com>
2022-05-26 10:38:39 +0530

    dashboard update

--- yellowcoin/settings.py
+++ yellowcoin/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'yellowcoin.urls'
@@ -145,14 +146,16 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+'''STATICFILES_DIRS = [BASE_DIR / 'static']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")'''
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
