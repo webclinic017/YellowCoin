c6cf737621ec59cd5b08221427b3ee5b9e0c4468
TheJagStudio <81472856+TheJagStudio@users.noreply.github.com>
2022-05-28 14:38:43 +0530

    make it better

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'dashboard'
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('running_orders/', views.running_orders, name='running_orders'),
+    path('exected_orders/', views.exected_order, name='exected_order'),
+    path('summary_reports/', views.summary_reports, name='summary_reports'),
     path('trade_entry/', views.trade_entry, name='trade_entry'),
+    path('M2M_alerts/', views.M2M, name='M2M'),
+    path('user/', views.user, name='user'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def dashboard(request):
     current_user = request.user
-    if current_user.username == 'admin':
+    if current_user.is_superuser:
         return render(request, 'dashboard_trades.html', {'current_user': current_user})
     else:
         return render(request, 'user_dashboard_trades.html', {'current_user': current_user})
@@ -17,3 +17,38 @@ def trade_entry(request):
     current_user = request.user
     if current_user.is_superuser:
         return render(request, 'dashboard_trade_entry.html', {'current_user': current_user})
+
+
+@login_required
+def exected_order(request):
+    current_user = request.user
+    if current_user.is_superuser:
+        return render(request, 'dashboard_executed_orders.html', {'current_user': current_user})
+
+
+@login_required
+def running_orders(request):
+    current_user = request.user
+    if current_user.is_superuser:
+        return render(request, 'dashboard_running_orders.html', {'current_user': current_user})
+
+
+@login_required
+def summary_reports(request):
+    current_user = request.user
+    if current_user.is_superuser:
+        return render(request, 'dashboard_summary_report.html', {'current_user': current_user})
+
+
+@login_required
+def M2M(request):
+    current_user = request.user
+    if current_user.is_superuser:
+        return render(request, 'dashboard_m2m.html', {'current_user': current_user})
+
+
+@login_required
+def user(request):
+    current_user = request.user
+    if current_user.is_superuser:
+        return render(request, 'dashboard_user.html', {'current_user': current_user})
